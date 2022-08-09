ddf5fcd641bb639bb8d02c5acd95f84260f1a3d7
Maiyoj <valentinemaiyo1995@gmail.com>
2022-08-09 13:38:59 +0300

    [initial] commit -m

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './index.css';
+import Item from './components/Items';
+
+function App() {
+  return (
+    <div className="">
+      <Item />
+    
+    </div>
+  );
+}
+
+export default App;
