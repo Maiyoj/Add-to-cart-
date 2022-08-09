c12252181c04740d4dabe1343c415cb65c0a7457
Maiyoj <valentinemaiyo1995@gmail.com>
2022-08-09 14:45:41 +0300

    [add] navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,12 @@
+import {Link} from  "react-router-dom";
+
 function Navbar(){
     return(
         <>
         <nav>
+            <Link to={"/"} exact="true">Home</Link>
+            <Link to={"/about"} exact="true">About</Link>
+            <Link to={"/items"} exact="true">Items</Link>
             
         </nav>
         </>
