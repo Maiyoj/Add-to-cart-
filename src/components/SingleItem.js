963c567d5220b7e1caf96744f4f1c250878181f3
Maiyoj <valentinemaiyo1995@gmail.com>
2022-08-10 19:10:38 +0300

    [add] style to button

--- src/components/SingleItem.js
+++ src/components/SingleItem.js
@@ -3,9 +3,11 @@ function SingleItem({name, price, image}){
         <>
         <div className="flex flex-col  bg-indigo-500 shadow-lg shadow-indigo-500/50 ml-9 mt-8">
         <img src={image} alt="Items"  className="h-60 w-60 "/>
-        <p>{price}</p>
-        <p>{name}</p>
-        <button>Add to Cart</button>
+        <div className="ml-5">
+        <p>Price: {price}</p>
+        <p>Name: {name}</p>
+        <button className="bg-black w-32 p-4 text-white rounded-md m-2">Add to Cart</button>
+        </div>
         </div>
         </>
     )
